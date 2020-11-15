dbc80feecffa86d31af02ff7b30783a9da2695d8
vivekmalik621 <vivekmalik621@gmail.com>
2020-11-15 20:58:34 +0100

    Test drafts for Single and Combo order processing

---
+++ src/test/java/com/org/promotionengine/service/OrderComboPromotionTypeTest.java
@@ -0,0 +1,21 @@
+package com.org.promotionengine.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.org.promotionengine.repo.PromotionRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderComboPromotionTypeTest {
+
+	
+	 @Mock
+	    private PromotionRepository promotionRepository;
+
+	    @Test
+	    public void processComboPromotionType() {
+
+	    }
+}
---
+++ src/test/java/com/org/promotionengine/service/OrderSinglePromotionTypeTest.java
@@ -0,0 +1,19 @@
+package com.org.promotionengine.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderSinglePromotionTypeTest {
+
+	@InjectMocks
+    OrderSinglePromotionType singlePromotionType;
+
+    @Test
+    public void testSinglePromotionsScenario1() {
+    	
+    }
+	
+}
